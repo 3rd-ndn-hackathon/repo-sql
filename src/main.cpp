035c09eca4a144d1d381db98de90cfdb44ce6d37
Junxiao Shi <git@mail1.yoursunny.com>
2016-11-05 22:52:49 +0000

    delete BulkInsertAcceptor

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include "database-conn.hpp"
 #include "query-processor.hpp"
-#include "bulk-insert-acceptor.hpp"
 
 namespace reposql {
 
@@ -13,9 +12,6 @@ main(int argc, char** argv)
   Face face(io);
   QueryProcessor qp(face, db);
   qp.addTopPrefix("/repo");
-
-  boost::asio::ip::tcp::endpoint ep(boost::asio::ip::address_v4::loopback(), 7376);
-  BulkInsertAcceptor bulk(io, ep, db);
 
   io.run();
   return 0;
